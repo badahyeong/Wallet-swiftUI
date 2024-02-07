e833eb8c53866816a7d3d5bcbb16d91cfbb9249a
아영 <ahyeong@sinest.co.kr>
2024-02-07 11:16:44 +0900

    Initial Commit

---
+++ Wallet-swiftUI/Wallet_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_swiftUIApp.swift
+//  Wallet-swiftUI
+//
+//  Created by 바다영 on 2024/02/07.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
